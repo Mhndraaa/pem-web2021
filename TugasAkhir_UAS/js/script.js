c6c5d139315824efe2d603c66cd4a971e09d8f8b
Mhndraaa <92259807+Mhndraaa@users.noreply.github.com>
2022-01-20 22:44:30 +0800

    Tugas Akhir UAS Mahendra Kumar
    Tugas Akhir UAS Mahendra Kumar

---
+++ TugasAkhir_UAS/js/script.js
@@ -0,0 +1,24 @@
+const txtElement = ['Konsultasi', 'Terapi', 'Ikuti Program Pelatihan'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.lenght){
+		count = 0;
+	}
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})();
